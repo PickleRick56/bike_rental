3fdce543974fe3a6b8814a8f1780e12084cde02c
Name <you@gmail.com>
2023-08-06 18:25:07 +0500

    Redux toolkit added

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,18 @@
 import { NavLink } from "react-router-dom";
 
+import { addTodo } from "./todoSlice";
+import { useSelector, useDispatch } from "react-redux";
+
 function Header() {
+    const dispatch = useDispatch();
+    const retrievedFromStore = useSelector((state) => state.todo.tasks);
+    console.log(retrievedFromStore);
+
     return (
         <div>
+            {/* {отправить данные в store по нажатиею накнопку} */}
+            <button onClick={() => dispatch(addTodo("dog"))}>click</button>
+
             <h1>This is the home header</h1>
             <NavLink to="/">Click to view our home page</NavLink>
             <NavLink to="/about">Click to view our about page</NavLink>
